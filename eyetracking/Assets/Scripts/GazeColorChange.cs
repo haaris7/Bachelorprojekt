6d2a1b8c154a9bdd5832a268c98b0895130230a0
Haaris Imtiaz <haarisimtiaz@gmail.com>
2023-04-26 15:42:34 +0200

    added gazecolor

---
+++ eyetracking/Assets/Scripts/GazeColorChange.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GazeColorChange : MonoBehaviour
+{
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color gazeColor = Color.red;
+
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    public void OnGazeEnter()
+    {
+        Debug.Log("OnGazeEnter called. Changing color to: " + gazeColor);
+        material.color = gazeColor;
+    }
+
+    public void OnGazeExit()
+    {
+        Debug.Log("OnGazeExit called. Changing color to: " + normalColor);
+        material.color = normalColor;
+    }
+}
