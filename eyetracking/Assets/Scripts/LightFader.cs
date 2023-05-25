55900b831ce45ca2cffa2df8fa287c2f9dd907ad
haarisi <haarisimtiaz@gmail.com>
2023-05-26 01:10:35 +0200

    task3 almost done

--- eyetracking/Assets/Scripts/LightFader.cs
+++ eyetracking/Assets/Scripts/LightFader.cs
@@ -1,70 +1,47 @@
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
 public class LightFader : MonoBehaviour
 {
-    public Material sphereMaterial; // The material of the sphere
+    public Material sphereMaterial;
     private Light light;
     private float targetIntensity;
-    private float fadeSpeed;  // The fading speed, which will be randomized
-    public float minFadeSpeed = 0.05f*1000f;  // The minimum possible fade speed
-    public float maxFadeSpeed = 0.5f*1000f;  // The maximum possible fade speed
-    public float minStartDelay = 0f*1000f;  // The minimum start delay
-    public float maxStartDelay = 5f*1000f;  // The maximum start delay
-    public float minHoldTime = 0.5f*1000f; // Minimum hold time before changing intensity
-    public float maxHoldTime = 5f*1000f; // Maximum hold time before changing intensity
-    public float minIntensity = 0f*1000f; // Minimum light intensity
-    public float maxIntensity = 1f*1000f; // Maximum light intensity
+    private float initialIntensity;
+    private float fadeSpeed;
+    private float lerpTime;
 
     void Start()
     {
-        light = GetComponent<Light>();
+        sphereMaterial = new Material(sphereMaterial);
+        GetComponent<MeshRenderer>().material = sphereMaterial;
+        light = transform.Find("Point Light").GetComponent<Light>();
+        initialIntensity = light.intensity;
+        targetIntensity = Random.Range(0f, 1f);
+        fadeSpeed = Random.Range(0.02f, 0.2f);
+        lerpTime = 0;
 
-        // Change the random seed based on the sphere's initial position
-        Random.InitState(transform.position.GetHashCode());
-
-        // Randomize the initial intensity and target intensity
-        float initialIntensity = Random.Range(minIntensity, maxIntensity);
-        light.intensity = initialIntensity;
-        targetIntensity = Random.Range(minIntensity, maxIntensity);
-
-        // Randomize the fade speed
-        fadeSpeed = Random.Range(minFadeSpeed, maxFadeSpeed);
-
-        // Start the ChangeIntensity coroutine with a random delay
-        float startDelay = Random.Range(minStartDelay, maxStartDelay);
-        StartCoroutine(StartChangeIntensity(startDelay));
+        StartCoroutine(StartAfterDelay(Random.Range(1, 4)));
     }
 
-    IEnumerator StartChangeIntensity(float delay)
+    IEnumerator StartAfterDelay(int delay)
     {
-        // Wait for the specified delay
         yield return new WaitForSeconds(delay);
 
-        // Then start changing the intensity
         while (true)
         {
-            // Smoothly change the light's intensity towards the target intensity.
-            light.intensity = Mathf.Lerp(light.intensity, targetIntensity, Time.deltaTime * fadeSpeed);
-
-            // Set the material's color based on the light's intensity.
+            light.intensity = Mathf.SmoothStep(initialIntensity, targetIntensity, lerpTime);
             sphereMaterial.color = new Color(light.intensity, light.intensity, light.intensity, 1);
 
-            // If we've reached the target intensity, wait a random amount of time before changing the target.
-            if (Mathf.Approximately(light.intensity, targetIntensity))
-            {
-                targetIntensity = Random.Range(minIntensity, maxIntensity);
-                float holdTime = Random.Range(minHoldTime, maxHoldTime);
-                yield return new WaitForSeconds(holdTime);
-            }
+            lerpTime += Time.deltaTime * fadeSpeed;
 
-            // Also randomly decide to change the target intensity before reaching it.
-            if (Random.value < 0.01f)
+            if (lerpTime >= 1)
             {
-                targetIntensity = Random.Range(minIntensity, maxIntensity);
+                lerpTime = 0;
+                initialIntensity = light.intensity;
+                targetIntensity = Random.Range(0f, 1f);
+                fadeSpeed = Random.Range(0.02f, 0.2f);
             }
-
-            // Wait for the next frame
+            
             yield return null;
         }
     }
